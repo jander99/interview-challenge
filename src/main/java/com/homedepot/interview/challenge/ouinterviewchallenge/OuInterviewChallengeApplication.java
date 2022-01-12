b2f73293f6501c6696ef83ad052541ad01fffa1f
Jeff Anderson <jeff@homedepot.com>
2022-01-12 16:16:14 -0500

    Initial Commit of Spring Boot Starter App

---
+++ src/main/java/com/homedepot/interview/challenge/ouinterviewchallenge/OuInterviewChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.homedepot.interview.challenge.ouinterviewchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuInterviewChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuInterviewChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homedepot/interview/challenge/ouinterviewchallenge/OuInterviewChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homedepot.interview.challenge.ouinterviewchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuInterviewChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
