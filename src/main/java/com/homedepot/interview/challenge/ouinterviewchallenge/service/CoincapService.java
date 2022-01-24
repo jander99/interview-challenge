d3c61079f769b6923090f109f7e4fda0d0c334ef
Jeff Anderson <jeff@homedepot.com>
2022-01-24 10:39:56 -0500

    Add a RestTemplate implementation

--- src/main/java/com/homedepot/interview/challenge/ouinterviewchallenge/service/CoincapService.java
+++ src/main/java/com/homedepot/interview/challenge/ouinterviewchallenge/service/CoincapService.java
@@ -1,12 +1,17 @@
 package com.homedepot.interview.challenge.ouinterviewchallenge.service;
 
+import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CoincapAsset;
+import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CoincapAssetWrapper;
 import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CryptoExchangeRate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.NoSuchElementException;
 
 
 @Service
@@ -31,6 +36,31 @@ public class CoincapService {
         // TODO: Implement the necessary logic and calls to
         //  Coincap that might be needed to satisfy requirements.
         return null;
+    }
+
+
+    /**
+     * Searches the Coincap API for a token by Symbol name
+     * @param tokenSymbol
+     * @return a Coincap Asset
+     * @throws NoSuchElementException
+     */
+    private CoincapAsset findTokenFromSymbol(String tokenSymbol) {
+
+        String assetSearchUrl = "/v2/assets?search=" + tokenSymbol;
+        CoincapAssetWrapper returnValue = coincapRestTemplate
+            .exchange(coincapBaseUrl + assetSearchUrl,
+                HttpMethod.GET,
+                getCoincapHeaders(),
+                CoincapAssetWrapper.class)
+            .getBody();
+
+        return returnValue
+            .getData()
+            .stream()
+            .filter(a -> a.getSymbol().equalsIgnoreCase(tokenSymbol))
+            .findAny()
+            .orElseThrow();
     }
 
     /**
